9fd96e1fbce6e357c3aba0701a3e8ad0afcd0362
yong940221 <yjh94@hotmail.com>
2023-05-08 12:38:29 +0800

    Generate uuid before create

--- publish/Models/BaseModel.php
+++ publish/Models/BaseModel.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Ramsey\Uuid\Uuid;
 
 class BaseModel extends Model
 {
@@ -26,5 +27,14 @@ class BaseModel extends Model
     public static function getUpdatable()
     {
         return static::$updatable;
+    }
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            $model->id = Uuid::uuid4()->toString();
+        });
     }
 }
